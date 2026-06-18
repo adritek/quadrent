5d8668ce85fe481cd76509ba6983d89d363d774f
Adrian Rogers <adritek@gmail.com>
2026-06-18 16:44:46 +0200

    chore: use consistent env variables, add 1 line npm to run front and backends

--- backend/src/db/client.ts
+++ backend/src/db/client.ts
@@ -1,7 +1,7 @@
 import { response } from 'express';
 import Nano from 'nano';
 
-const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = process.env.COUCHDB_URL;
 
 if (!DATABASE_URL) {
   throw new Error('DATABASE_URL is not defined in environment variables');
